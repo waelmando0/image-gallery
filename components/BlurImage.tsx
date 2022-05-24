ba4bc0c32176d1108cb9dfe31896e74f9bd3c31d
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2022-05-24 05:43:47 +0200

    image gallery project

---
+++ components/BlurImage.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { useState } from "react";
+
+function cn(...classs: string[]) {
+  return classs.filter(Boolean).join(" ");
+}
+
+type Image = {
+  id: number;
+  href: string;
+  imageSrc: string;
+  name: string;
+  username: string;
+};
+
+const BlurImage = ({ image }: { image: Image }) => {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            "duration-700 ease-in-out group-hover:opacity-75",
+            isLoading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+  );
+};
+
+export default BlurImage;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  // we want to have an allow list of domains that we're telling next.js hey you can optimize theses images
+  // this is for good security in our application to prevent people from trying to use our next.js server to optimize any image
+  images: {
+    domains: ['pbs.twimg.com'],
+  },
 }
