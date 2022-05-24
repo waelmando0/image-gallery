20c36de725151c64d9d15996d3a742ef1ff61094
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2022-05-24 14:10:03 +0200

    change sm width

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
-import { useState } from "react";
+import Image from 'next/image'
+import { useState } from 'react'
 
 function cn(...classs: string[]) {
-  return classs.filter(Boolean).join(" ");
+  return classs.filter(Boolean).join(' ')
 }
 
 type Image = {
-  id: number;
-  href: string;
-  imageSrc: string;
-  name: string;
-  username: string;
-};
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
 
 const BlurImage = ({ image }: { image: Image }) => {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true)
 
   return (
     <a href={image.href} className="group">
@@ -24,11 +24,12 @@ const BlurImage = ({ image }: { image: Image }) => {
           src={image.imageSrc}
           layout="fill"
           objectFit="cover"
+          priority
           className={cn(
-            "duration-700 ease-in-out group-hover:opacity-75",
+            'duration-700 ease-in-out group-hover:opacity-75',
             isLoading
-              ? "scale-110 blur-2xl grayscale"
-              : "scale-100 blur-0 grayscale-0"
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
           )}
           onLoadingComplete={() => setLoading(false)}
         />
@@ -36,7 +37,7 @@ const BlurImage = ({ image }: { image: Image }) => {
       <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
-  );
-};
+  )
+}
 
-export default BlurImage;
+export default BlurImage
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,37 @@
-import { createClient } from "@supabase/supabase-js";
-import BlurImage from "../components/BlurImage";
+import { createClient } from '@supabase/supabase-js'
+import BlurImage from '../components/BlurImage'
 
 type Image = {
-  id: number;
-  href: string;
-  imageSrc: string;
-  name: string;
-  username: string;
-};
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {images.map((image) => (
-          <BlurImage key={image.id} image={image} />
-        ))}
+    <div className="mx-auto max-w-sm px-5 sm:max-w-2xl sm:px-6 lg:max-w-6xl lg:px-8">
+      <div className="py-16 sm:py-24">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {images.map((image) => (
+            <BlurImage key={image.id} image={image} />
+          ))}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 export async function getStaticProps() {
   const suabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
-  );
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
 
-  const { data } = await suabaseAdmin.from("images").select("*").order("id");
+  const { data } = await suabaseAdmin.from('images').select('*').order('id')
 
   return {
     props: { images: data },
-  };
+  }
 }
